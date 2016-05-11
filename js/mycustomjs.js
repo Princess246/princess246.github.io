752c71428fb3eabc1f1bed4ef979270790c970ec
princess246 <mcdonald246@yahoo.com>
2016-05-11 04:07:42 +0000

    Individual Project

---
+++ js/mycustomjs.js
@@ -0,0 +1,11 @@
+/*
+$(document).ready(function(){
+    $(".dropdown-toggle").dropdown();
+});
+
+*/
+$(document).ready(function() {
+$("modalfade").click(function(){
+    $("modalfade").animate({left: '250px'});
+}); 
+}
